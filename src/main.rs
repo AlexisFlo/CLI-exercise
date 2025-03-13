7a747a50d1475af5c66194ea8a2cda87b4f204ed
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-03-13 12:49:37 -0600

    feat: add test
    add directory test and add cli file, add dev dependencies in the
    cargo.toml file

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,29 @@
 use anyhow::{Context, Result};
-use std::fs::File;
-use std::io::{ BufRead, BufReader};
 use clap::Parser;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
-    // The pattern to look for
+    /// The pattern to look for
     pattern: String,
-    // The path to the file to read
+    /// The path to the file to read
     path: std::path::PathBuf,
 }
 
-// Exercise for the reader: This is not the best implementation: 
-// It will read the whole file into memory – however large the file may be. 
-// Find a way to optimize it! (One idea might be to use a BufReader instead
-// of read_to_string().)
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let args = Cli::parse();
-    let file = File::open(&args.path)
-        .with_context(|| format!("could not rea file `{}`", args.path.display()))?;
 
-    let reader = BufReader::new(file);
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
-    for line in reader.lines() {
-        let line = line?;
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    grrs::find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
